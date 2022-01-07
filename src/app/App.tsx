cbc351cf301db48049f6d4289dd19f3bc31ab580
YoshiCity <pawel.mirys@gmail.com>
2022-01-07 14:18:41 +0100

    init files

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.scss';
+import { Footer } from './components/Footer/Footer';
+import { Nav } from './components/Nav/Nav';
+import { About } from './sections/About/About';
+import { Contact } from './sections/Contact/Contact';
+import { Hero } from './sections/Hero/Hero';
+import { Projects } from './sections/Projects/Projects';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <About />
+      <Projects />
+      <Contact />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
